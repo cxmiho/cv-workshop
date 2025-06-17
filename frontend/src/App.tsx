162f59c3abc4726ef2a913614d2d868acc88200e
cxmiho <miho@computas.com>
2025-06-17 11:19:37 +0200

    Comment out front-end

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,18 +4,21 @@ import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 
 export default function App() {
+  /* 
   return (
+    TODO Oppgave 1.2: Legg til en ny route i App.tsx for Erfaringer 
     <BrowserRouter>
       <div className="app">
         <MainHeader />
         <main>
           <Routes>
             <Route path="/" element={<Home />} />
-            {/*TODO Oppgave 1.1: Legg til en ny route i App.tsx for Erfaringer*/}
+            {TODO Oppgave 1.1: Legg til en ny route i App.tsx for Erfaringer}
             <Route path="*" element={<NotFound />} />
           </Routes>
         </main>
       </div>
     </BrowserRouter>
   );
+   */
 }
