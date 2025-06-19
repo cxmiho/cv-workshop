1b9c039877002d58bdc79d03459c1ba671979b91
cxmiho <miho@computas.com>
2025-06-19 09:30:04 +0200

    Update front-end

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,23 +2,22 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainHeader from "./components/main-header/main-header";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
+import Experiences from "./pages/Experiences";
 
 export default function App() {
-  /* 
   return (
-    TODO Oppgave 1.2: Legg til en ny route i App.tsx for Erfaringer 
     <BrowserRouter>
       <div className="app">
         <MainHeader />
         <main>
           <Routes>
             <Route path="/" element={<Home />} />
-            {TODO Oppgave 1.1: Legg til en ny route i App.tsx for Erfaringer}
+            {/*TODO Oppgave 1.1: Legg til en ny route i App.tsx for Erfaringer*/}
+            <Route path="/experiences" element={<Experiences />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </main>
       </div>
     </BrowserRouter>
   );
-   */
 }
